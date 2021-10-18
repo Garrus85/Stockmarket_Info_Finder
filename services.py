960acfbce30b72e3444964c912a36ddfd1bb5e17
Jonny RS <jonnyrs@protonmail.com>
2021-10-18 20:27:28 +0100

    Refactored some code

--- services.py
+++ services.py
@@ -9,15 +9,13 @@ def intro():
 
 def get_ticker() -> object:
     ticker_request = input("Please input a ticker symbol? ")
-    ticker = yf.Ticker(ticker_request.upper())
-    return ticker
+    return yf.Ticker(ticker_request.upper())
 
 
 def ticker_date_int(ticker) -> object:
     time = input("Please specify a time period required? \n [1d][5d][1mo][3mo][6mo][1y][2y][5y][10y][ytd][max] \n")
     int_req = input("Please specify the interval? \n [1m][2m][5m][15m][30m][60m][90m][1h][1d][5d][1wk][1mo][3mo] \n")
-    ticker_historical = ticker.history(period=time, interval=int_req)
-    return ticker_historical
+    return ticker.history(period=time, interval=int_req)
 
 
 def information(t):
